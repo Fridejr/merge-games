7d65ea3e050eaa60b6b88fa59593c6195fd345a3
Fridejr <antoniotorodaw@gmail.com>
2024-06-18 12:08:02 +0200

    otra

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://merge-games-production-80fc.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
